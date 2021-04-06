2ea65bc35463c144464862a43fbb39857b9548e4
Jiezhen Yi <yijiezhen@gmail.com>
2021-04-05 20:41:10 -0700

    fix gpg

--- project.clj
+++ project.clj
@@ -71,7 +71,7 @@
               [["clojars" {:url "https://clojars.org/repo/"
                            :username :env/clojars_username
                            :password :env/clojars_password
-                           :signing {:gpg-key "clojars@parkside.app"}}]]}
+                           :signing {:gpg-key "jiezhen@parkside.app"}}]]}
 
              ;; The dev profile - non-deployment configuration
              :dev
